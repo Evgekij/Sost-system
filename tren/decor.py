7de00f07dd0ee14cfbeb71a427c804df8a5f13ef
Evgekij <Evgekijs@gmail.com>
2023-10-26 17:29:57 +0300

    fixed the decorator's work

--- tren/decor.py
+++ tren/decor.py
@@ -1,10 +1,15 @@
 import json
-
+from functools import wraps
 
 def caller(file_name):
     def arhivar(func):
-        data = func()
-        with open(file_name, "w") as f:
-            json.dump(data, f, indent = 4)
-        return func
+        @wraps(func)
+        def dopf():
+            data = func()
+            with open(file_name, "w") as f:
+                json.dump(data, f, indent = 4)
+            return data
+        
+        return dopf
+    
     return arhivar
